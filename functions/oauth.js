69a59141ac90c00d0cc95f39e3b4bee76de52585
geedaha <geedaha237@gmail.com>
2025-06-06 12:23:59 +0300

    Исправлен oauth.js: убраны импорты itty-router-extras и node-fetch, используем глобальный fetch

--- functions/oauth.js
+++ functions/oauth.js
@@ -1,34 +1,31 @@
-import { json } from 'itty-router-extras'
-import fetch from 'node-fetch'
-
 export async function onRequestPost({ request, env }) {
-  // Читаем JSON из тела POST-запроса
-  const { code } = await request.json()
+  // Парсим тело запроса как JSON
+  const body = await request.json();
+  const code = body.code;
 
   if (!code) {
     return new Response(
       JSON.stringify({ error: 'Missing code parameter' }),
-      { status: 400 }
-    )
+      { status: 400, headers: { 'Content-Type': 'application/json' } }
+    );
   }
 
-  // Делаем POST на GitHub, чтобы обменять code -> access_token
+  // Делаем запрос на GitHub для обмена code -> access_token
   const response = await fetch('https://github.com/login/oauth/access_token', {
     method: 'POST',
-    headers: { 
-      'Content-Type': 'application/json', 
-      'Accept': 'application/json' 
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
     },
     body: JSON.stringify({
       client_id: env.GITHUB_CLIENT_ID,
       client_secret: env.GITHUB_CLIENT_SECRET,
       code: code
     })
-  })
+  });
 
-  const data = await response.json()
-  // Возвращаем клиенту JSON с { access_token, scope, token_type }
+  const data = await response.json();
   return new Response(JSON.stringify(data), {
     headers: { 'Content-Type': 'application/json' }
-  })
+  });
 }
