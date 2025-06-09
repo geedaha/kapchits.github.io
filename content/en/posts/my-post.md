466835a6dd3806c3cc4b9730ebace1c800d96f2e
Your Name <you@example.com>
2025-06-09 23:08:05 +0300

    deploy: refresh site 2025-06-09T23:08:05.0149670+03:00

--- content/en/posts/my-post.md
+++
@@ -1,5 +0,0 @@
-+++
-date = '2025-06-09T22:40:21+03:00'
-draft = true
-title = 'My Post'
-+++
--- functions/oauth.js
+++
@@ -1,8 +0,0 @@
-﻿export default async function onRequest(context) {
-  const { request } = context;
-  // Всегда возвращаем JSON с методом, чтобы проверить, что функция вообще вызывается
-  return new Response(
-    JSON.stringify({ ok: true, method: request.method }),
-    { status: 200, headers: { 'Content-Type': 'application/json' } }
-  );
-}
